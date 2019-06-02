b48b20ff0b3e927d8debc4fe94ec505cab5a1a82
Velmiskin, Ilya Sergeevich <ilya@gatech.edu>
2019-06-02 20:34:36 +0800

    Added Person2

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person2.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person2.java
@@ -3,9 +3,9 @@ package edu.gatech.oad.antlab.person;
 /**
  *  A simple class for person 2
  *  returns their name and a
- *  modified string 
+ *  modified string
  *
- * @author Bob
+ * @author Ilya
  * @version 1.1
  */
 public class Person2 {
@@ -30,15 +30,23 @@ public class Person2 {
 	 * @return the modified string
 	 */
 	private String calc(String input) {
-	  //Person 2 put your implementation here
-	  return null;
-	}
+      String output = null;
+      String orig = input;
+      for (int i = 0; i < orig.length(); i++) {
+          int rand = (int) Math.random() * input.length();
+          output += input.charAt(rand);
+          if (i != 0) {
+              input = input.substring(0, rand) + input.substring(rand + 1);
+          }
+      }
+      return output;
+    }
 	/**
 	 * Return a string rep of this object
 	 * that varies with an input string
 	 *
 	 * @param input the varying string
-	 * @return the string representing the 
+	 * @return the string representing the
 	 *         object
 	 */
 	public String toString(String input) {
